64528fb98d9c8b176d95aaf8832a322ecd3dc952
Anvesh2107 <75076209+Anvesh2107@users.noreply.github.com>
2026-08-29 12:20:39 -0500

    Solve 1. Two Sum [Easy] — 2 ms (99.3%)

---
+++ 0001-two-sum/solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> seen=new HashMap<>();
+
+        for(int i=0;i<=nums.length;i++){
+            if(seen.containsKey(nums[i])){
+                return new int[]{i,seen.get(nums[i])};
+            }
+            seen.put(target-nums[i],i);
+        }
+        return new int[]{-1,-1};
+        
+    }
+}
